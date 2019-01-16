3aad020009bd0942974d5a6893b1a54d183bc81f
stephenb <stephen.bartholomew@paxport.net>
2019-01-16 13:17:54 +0000

    Add exercises

---
+++ src/main/java/io/cucumber/shouty/Loaf.java
@@ -0,0 +1,16 @@
+package io.cucumber.shouty;
+
+public class Loaf {
+
+    public int flour;
+    public int water;
+    public int salt;
+    public int yeast;
+
+    public Loaf(int flour, int water, int salt, int yeast) {
+        this.flour = flour;
+        this.water = water;
+        this.salt = salt;
+        this.yeast = yeast;
+    }
+}
